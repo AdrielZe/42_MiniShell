222c38b31f6f33713c1e4128696c80277c504915
Adriel Silveira De Oliveira <asilveir@student.42sp.org.br>
2025-01-10 12:43:56 -0300

    Makefile

---
+++ headers/main.h
@@ -0,0 +1,12 @@
+# ifndef MAIN_H
+# define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "../libft/libft.h"
+
+char	*process_env_var(char *input);
+
+#endif
