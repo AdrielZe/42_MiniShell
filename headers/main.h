2198d0811ebbae084ed6af31193cac1cc32335ff
Adriel Silveira De Oliveira <asilveir@student.42sp.org.br>
2025-01-13 11:37:26 -0300

    Feat: comentando funcoes utilizadas ao lado das libs

--- headers/main.h
+++ headers/main.h
@@ -1,11 +1,23 @@
-# ifndef MAIN_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asilveir <asilveir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 11:28:59 by asilveir          #+#    #+#             */
+/*   Updated: 2025/01/13 11:36:43 by asilveir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
 # define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include "../libft/libft.h"
+# include <stdio.h> //printf
+# include <stdlib.h> //malloc, free
+# include <readline/readline.h> //readline
+# include <readline/history.h> //readline history
+# include "../libft/libft.h" //libft
 
 char	*process_env_var(char *input);
 
