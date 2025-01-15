f12f20678378a89f52561252d6e8fe06ccbd0117
Adriel Silveira De Oliveira <asilveir@student.42sp.org.br>
2025-01-15 16:38:06 -0300

    [alt] including main.h in tokenize.h

--- headers/tokenize.h
+++ headers/tokenize.h
@@ -3,18 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   tokenize.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: victda-s <victda-s@student.42.fr>          +#+  +:+       +#+        */
+/*   By: asilveir <asilveir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 12:12:24 by victda-s          #+#    #+#             */
-/*   Updated: 2025/01/15 12:46:57 by victda-s         ###   ########.fr       */
+/*   Updated: 2025/01/15 16:31:00 by asilveir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TOKENIZE_H
 # define TOKENIZE_H
 
-# include <stdlib.h>
-# include "../libft/libft.h"
+# include "../headers/main.h"
+
+typedef enum e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIRECT_IN,
+	TOKEN_REDIRECT_OUT,
+	TOKEN_APPEND,
+	TOKEN_HEREDOC,
+	TOKEN_ENV_VAR,
+	TOKEN_UNKNOWN
+}	t_token_type;
+
+typedef struct s_tokens
+{
+	t_token_type	type;
+	char			*value;
+	struct s_tokens	*next;
+}	t_tokens;
+
 int	count_if(size_t *count, const char *s, char c);
 
 #endif
