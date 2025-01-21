f0f16cd4db19714b56a36a0ab483df4015e6aaf4
AdrielZe <oadriel14@gmail.com>
2025-01-21 17:07:25 -0300

    comentando arquivo com bug

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -3,27 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asilveir <asilveir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 14:32:59 by asilveir          #+#    #+#             */
-/*   Updated: 2024/10/23 17:07:09 by asilveir         ###   ########.fr       */
+/*   Updated: 2025/01/21 15:30:35 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
-{
-	size_t	i;
+// void	*ft_memchr(const void *s, int c, size_t n)
+// {
+// 	size_t	i;
 
-	i = 0;
-	while (i < n)
-	{
-		if (((unsigned char *)s)[i] == (unsigned char) c)
-		{
-			return ((unsigned char *) &s[i]);
-		}
-		i++;
-	}
-	return (NULL);
-}
+// 	i = 0;
+// 	while (i < n)
+// 	{
+// 		if (((unsigned char *)s)[i] == (unsigned char) c)
+// 		{
+// 			return ((unsigned char *) &s[i]);
+// 		}
+// 		i++;
+// 	}
+// 	return (NULL);
+// }
