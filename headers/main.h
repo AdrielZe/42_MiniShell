59780709ef7011719a6c3493f32a44fcc03796b4
AdrielZe <oadriel14@gmail.com>
2025-01-21 17:06:10 -0300

    adicionando tokens.h na main

--- headers/main.h
+++ headers/main.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: victda-s <victda-s@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 11:28:59 by asilveir          #+#    #+#             */
-/*   Updated: 2025/01/15 11:44:02 by victda-s         ###   ########.fr       */
+/*   Updated: 2025/01/21 14:13:38 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdlib.h> //malloc, free
 # include <readline/readline.h> //readline
 # include <readline/history.h> //readline history
+# include "tokenize.h"
 # include "../libft/libft.h" //libft
 
 char	*process_env_var(char *input);
