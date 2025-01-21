95afd183ebda64650d62dacf30b44afb597c9431
AdrielZe <oadriel14@gmail.com>
2025-01-21 17:08:54 -0300

    [feat] building ast function

--- src/parsing/build_ast.c
+++ src/parsing/build_ast.c
@@ -3,12 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   build_ast.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asilveir <asilveir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 18:32:49 by asilveir          #+#    #+#             */
-/*   Updated: 2025/01/20 18:49:07 by asilveir         ###   ########.fr       */
+/*   Updated: 2025/01/21 17:05:35 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../headers/parsing.h"
+
+void print_ast(t_ast_node *node, int level)
+{
+    if (!node)
+        return;
+
+    // Indentação para visualizar a hierarquia
+    for (int i = 0; i < level; i++)
+        printf("  ");
+    
+    // Exibir o tipo e valor do nó
+    printf("[%s: %s]\n", node->type == NODE_PIPE ? "PIPE" : "COMMAND", node->value);
+
+    // Recursão para o filho esquerdo, se houver
+    if (node->left)
+    {
+        for (int i = 0; i < level + 1; i++)  // Mais um nível de indentação para o filho esquerdo
+            printf("  ");
+        printf("[LEFT]\n");
+        print_ast(node->left, level + 1);
+    }
+
+    // Recursão para o filho direito, se houver
+    if (node->right)
+    {
+        for (int i = 0; i < level + 1; i++)  // Mais um nível de indentação para o filho direito
+            printf("  ");
+        printf("[RIGHT]\n");
+        print_ast(node->right, level + 1);
+    }
+}
+
+
+//char    test_ast()
 
 void free_ast(t_ast_node *node) 
 {
@@ -35,7 +71,7 @@ t_ast_node	*create_node(t_node_type type, char *value)
     return (node); 
 }
 
-t_ast_node	*build_ast(t_token *tokens)
+t_ast_node	*build_ast(t_tokens *tokens)
 {
 	t_ast_node	*root;
 	t_ast_node	*current;
@@ -47,11 +83,22 @@ t_ast_node	*build_ast(t_token *tokens)
 		if (tokens->type == TOKEN_PIPE)
 		{
 			t_ast_node	*pipe_node;
-			
-			pipe_node = create_node(NODE_PIPE, "|");
-			
 
-		}
-	}
+			pipe_node = create_node(NODE_PIPE, "|");
+            pipe_node->left = root;   // O comando anterior será o filho esquerdo do PIPE
+            root = pipe_node;
+            current = root;
+		} else if (tokens->type == TOKEN_COMMAND)
+        {
+            t_ast_node *command_node = create_node(NODE_COMMAND, tokens->value);
+            if (!root)
+                root = command_node;
+            else if (current)
+                current->right = command_node;
+            current = command_node;
+        }
+        tokens = tokens->next;
+	}    
+    print_ast(root, 0);
 	return (root);
 }
