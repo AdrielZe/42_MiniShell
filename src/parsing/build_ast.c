59df673aac0b3ebbbbeffe53395f178a15ab7c12
adrielze <oadriel14@gmail.com>
2025-01-22 17:20:37 -0300

    Norminetting and refactoring

--- src/parsing/build_ast.c
+++ src/parsing/build_ast.c
@@ -6,12 +6,13 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 18:32:49 by asilveir          #+#    #+#             */
-/*   Updated: 2025/01/21 18:25:47 by marvin           ###   ########.fr       */
+/*   Updated: 2025/01/22 17:03:38 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/parsing.h"
 
+// Essa função não será usada qnd o projeto ficar pronto, apenas para testes, por isso ela está fora da norma
 void print_ast(t_ast_node *node, int level)
 {
     if (!node)
@@ -43,35 +44,29 @@ void print_ast(t_ast_node *node, int level)
     }
 }
 
-
-// char    test_ast(t_ast  *node)
-// {
-    
-// }
-
-void free_ast(t_ast_node *node) 
+void	free_ast(t_ast_node *node)
 {
-    if (!node)                      
-        return ;
-    free_ast(node->left);      
-    free_ast(node->right);      
-    if (node->value)        
-        free(node->value);
-    free(node);
+	if (!node)
+		return ;
+	free_ast(node->left);
+	free_ast(node->right);
+	if (node->value)
+		free(node->value);
+	free(node);
 }
 
 t_ast_node	*create_node(t_node_type type, char *value)
 {
-    t_ast_node	*node;
-    
-    node = malloc(sizeof(t_ast_node));
-    if (!node)
-        return (NULL);
-    node->type = type;
-    node->value = ft_strdup(value); 
-    node->left = NULL;
-    node->right = NULL;
-    return (node); 
+	t_ast_node	*node;
+
+	node = malloc(sizeof(t_ast_node));
+	if (!node)
+		return (NULL);
+	node->type = type;
+	node->value = ft_strdup(value);
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
 }
 
 t_ast_node	*build_ast(t_tokens *tokens)
@@ -84,54 +79,47 @@ t_ast_node	*build_ast(t_tokens *tokens)
 	while (tokens)
 	{
 		if (tokens->type == TOKEN_PIPE)
-		{
-			t_ast_node	*pipe_node;
-
-			pipe_node = create_node(NODE_PIPE, "|");
-            pipe_node->left = root;
-            root = pipe_node;
-            current = root;
-		} else if (tokens->type == TOKEN_COMMAND)
-        {
-            t_ast_node *command_node = create_node(NODE_COMMAND, tokens->value);
-            if (!root)
-                root = command_node;
-            else if (current)
-                current->right = command_node;
-            current = command_node;
-        }
-        tokens = tokens->next;
-	}    
-    print_ast(root, 0);
+			create_pipe_node(&root, &current);
+		else if (tokens->type == TOKEN_COMMAND)
+			create_command_node(&root, &current, tokens);
+		tokens = tokens->next;
+	}
+	print_ast(root, 0);
 	return (root);
 }
 
-char *concatenate_commands(t_ast_node *node) {
-    if (!node)
-        return NULL;
+char	*parse_commands(t_ast_node *node)
+{
+	char	*result;
+	char	*left_result;
+	char	*right_result;
 
-    char *result = malloc(1);
-    result[0] = '\0';
-
-    if (node->type == NODE_COMMAND) {
-        // logica para procurar o comando (funcao) , e se for valido, direcionar o retorno para um pipe e passar para o proximo processo
-        result = realloc(result, strlen(result) + strlen(node->value) + 1);
-        strcat(result, node->value);
-    }
-
-    if (node->left) {
-        char *left_result = concatenate_commands(node->left);
-        result = realloc(result, strlen(result) + strlen(left_result) + 1);
-        strcat(result, left_result);
-        free(left_result);
-    }
-
-    if (node->right) {
-        char *right_result = concatenate_commands(node->right);
-        result = realloc(result, strlen(result) + strlen(right_result) + 1);
-        strcat(result, right_result);
-        free(right_result);
-    }
-
-    return result;
+	left_result = NULL;
+	right_result = NULL;
+	if (!node)
+		return (NULL);
+	result = malloc(1);
+	result[0] = '\0';
+	if (node->type == NODE_COMMAND)
+	{
+	// logica para procurar o comando (funcao) ,
+	// e se for valido, direcionar o retorno para um pipe e passar para o proximo processo
+		result = ft_realloc(result, ft_strlen(result) + ft_strlen(node->value) + 1);
+		ft_strcat(result, node->value);
+	}
+	if (node->left)
+	{
+		left_result = parse_commands(node->left);
+		result = ft_realloc(result, ft_strlen(result) + ft_strlen(left_result) + 1);
+		ft_strcat(result, left_result);
+		free(left_result);
+	}
+	if (node->right)
+	{
+		right_result = parse_commands(node->right);
+		result = ft_realloc(result, ft_strlen(result) + ft_strlen(right_result) + 1);
+		ft_strcat(result, right_result);
+		free(right_result);
+	}
+	return (result);
 }
