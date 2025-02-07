c615b8243f33390100ec1aae63c5544040d1d7d2
Adriel Silveira De Oliveira <asilveir@student.42sp.org.br>
2025-02-07 17:19:39 -0300

    fixing '<<' memory leak

--- src/tokenize/tokenize.c
+++ src/tokenize/tokenize.c
@@ -47,10 +47,12 @@ size_t	ft_count_word(const char *s)
 	while (*s)
 	{
 		s += count_if(&count, s);
-		while (*s == '|' || (*s == '<' && *s++ == '<'))
+		while (*s == '|' || (*s == '<' && *(s + 1) == '<'))
 		{
 			delim_counter++;
 			s++;
+			if (*s)
+				s++;
 		}
 		count++;
 	}
