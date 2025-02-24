f86fd6094a5d1640b3b6ffbc974713a8545633f2
Adriel Silveira De Oliveira <asilveir@student.42sp.org.br>
2025-02-24 15:05:06 -0300

    almost finishing ajusts

--- src/parsing/command_execution_2.c
+++ src/parsing/command_execution_2.c
@@ -6,7 +6,7 @@
 /*   By: asilveir <asilveir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 13:06:54 by marvin            #+#    #+#             */
-/*   Updated: 2025/02/21 18:31:06 by asilveir         ###   ########.fr       */
+/*   Updated: 2025/02/24 15:04:50 by asilveir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,67 +21,94 @@ int is_directory(char *path)
 	return (0);
 }
 
+
+int is_file(const char *path)
+{
+    struct stat path_stat;
+
+    if (stat(path, &path_stat) != 0)
+        return (0);
+
+    return (S_ISREG(path_stat.st_mode));
+}
+
+static void	handle_command_node(t_ast_node *node, char **envp)
+{
+	char	old_char;
+	char *old_string;
+
+	if (!node->value || node->value[0] == '\0')
+		return ;
+	old_char = node->value[0];
+	old_string = ft_strdup(node->value);
+	node->value = process_env_var(node->value);
+	
+}
+
+static void	when_only_env_var(t_ast_node *node, char **envp, char *old_string)
+{
+	if (is_directory(node->value))
+	{
+		printf("minishell: %s: Is a directory\n", node->value);
+		return;
+	}
+	if (!is_file(node->value) && !search_valid_path(node->value, envp))
+	{
+		if (ft_strcmp(old_string, node->value) != 0)
+			printf("zsh: %s: No such file or directory\n", node->value);
+		else
+			return ;
+		return ;
+	}
+	execute_command(node->value, envp, node, 1);
+}
+
+static void check_if_is_cmd(t_ast_node *node, char **envp)
+{
+	if (ft_split(node->value, ' ')[0])
+	{
+		if (search_valid_path(ft_split(node->value, ' ')[0], envp) == NULL)
+		{
+			printf("minishell: %s: command not found\n", ft_split(node->value, ' ')[0]);
+			return ;
+		}
+	}
+	if (is_directory(node->value))
+	{
+		printf("minishell: %s: Is a directory\n", node->value);
+		return;
+	} else 
+		execute_command(node->value, envp, node, 0);
+}
+
 void handle_node_types(t_ast_node *node, char **envp, t_delim **delimiters)
 {
-    if (node->type == NODE_HEREDOC)
-    {
-        *delimiters = get_all_delimiters(node);
-        handle_heredoc(node, envp);
-    }
-    else if (node->type == NODE_COMMAND)
-    {
-        char old_char = node->value[0];
-        node->value = process_env_var(node->value);
+	char	old_char;
+	char	*old_string;
+	if (node->type == NODE_HEREDOC)
+	{
+		*delimiters = get_all_delimiters(node);
+		handle_heredoc(node, envp);
+	}
+	else if (node->type == NODE_COMMAND)
+	{
+		old_char = node->value[0];
+		if (!node->value || node->value[0] == '\0')
+			return ;
+		old_string = ft_strdup(node->value);
+		node->value = process_env_var(node->value);
 
-        if (old_char == '$')
-        {
-            // Verifica se o valor é um diretório
-            if (is_directory(node->value))
-            {
-                fprintf(stderr, "minishell: %s: Is a directory\n", node->value);
-                return;
-            }
-
-            // Tenta executar o valor como um comando
-            execute_command(node->value, envp, node, 1);
-        }
-        else if (ft_strchr(node->value, '/') != NULL)
-        {
-            if (ft_split(node->value, ' ')[0])
-            {
-                if (search_valid_path(ft_split(node->value, ' ')[0], envp) == NULL)
-                {
-                    printf("minishell: %s: command not found\n", ft_split(node->value, ' ')[0]);
-                    return;
-                }
-            }
-            execute_command(node->value, envp, node, 0);
-        }
-        else
-        {
-            execute_command(node->value, envp, node, 0);
-        }
-    }
+		if (old_char == '$')
+			when_only_env_var(node, envp, old_string);
+		else if (ft_strcmp(old_string, node->value) != 0)
+			check_if_is_cmd(node, envp);
+		else
+		{
+			if (ft_strchr(node->value, '$') != NULL)
+				execute_command(ft_split(node->value, ' ')[0], envp, node, 0);
+			else
+				execute_command(node->value, envp, node, 0);
+		}
+	}
 }
-	// else if (node->type == NODE_ENV_VAR)
-	// {
-	// 	node->value = process_env_var(node->value);
-	// 	if (access(node->value, F_OK) == 0) // Se o caminho existe
-	// 	{
-    	// 		if (access(node->value, X_OK) == -1) // Se não for executável
-    	// 		{
-       //  			if (is_directory(node->value)) // Se for um diretório
-       //      				fprintf(stderr, "minishell: %s: Is a directory\n", node->value);
-       //  			else
-       //      				fprintf(stderr, "minishell: %s: Permission denied\n", node->value);
-       // 			return;
-    	// 		}
-	// 	}
-	// 	else if (node->value[0] == '/') // Se não existe e começa com '/'
-	// 	{
-    	// 		fprintf(stderr, "minishell: %s: No such file or directory\n", node->value);
-    	// 		return;
-	// 	}  else
-	// 		execute_command(node->value, envp, node, 1);
-	// }
 
