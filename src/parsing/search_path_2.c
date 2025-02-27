644413689b251e09972790ac49e3507dce14b4a6
Victor Paulo Da Silva Garcia <victda-s@c3r12p5.42sp.org.br>
2025-02-27 00:41:26 -0300

    [fix]: acess built-ins

--- src/parsing/search_path_2.c
+++ src/parsing/search_path_2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   search_path_2.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asilveir <asilveir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: victda-s <victda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 17:27:34 by marvin            #+#    #+#             */
-/*   Updated: 2025/02/26 23:29:56 by asilveir         ###   ########.fr       */
+/*   Updated: 2025/02/27 00:40:52 by victda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,28 @@
 int	cd(char *argv[]);
 int	export(char *argv[], char **envp);
 
-static int if_cd(char *cmd, char *tokens[], char **envp)
+static int if_cd(char *cmd, char *tokens[], char **envp, t_ast_node *node)
 {
-	if(ft_strcmp(tokens[0], "cd") == 0)
+	if (!tokens || !tokens[0])
+		perror("Comando vazio\n");
+	else if(ft_strcmp(tokens[0], "cd") == 0)
+	{
 		if(cd(tokens))
 			return (1);
-	if(ft_strcmp(tokens[0], "export") == 0)
+	}
+	else if(ft_strcmp(tokens[0], "export") == 0)
+	{
 		if(export(tokens, envp))
 			return (1);
+	}
+	if(node->outfile)
+		close(node->outfile);
+	if(node->infile)
+		close(node->infile);
 	return (0);
 }
 static void	valid_outfile_and_path(char *cmd, t_ast_node *node, char *path)
 {
-	if (node->outfile)
-		dup2(node->outfile, STDOUT_FILENO);
-	if (node->infile)
-		dup2(node->infile, STDIN_FILENO);
 	if (!path)
 	{
 		ft_putstr_fd(cmd, STDERR_FILENO);
@@ -38,34 +44,11 @@ static void	valid_outfile_and_path(char *cmd, t_ast_node *node, char *path)
 	}
 }
 
-static void	execute_node_command(t_ast_node *node, char *cmd, char **envp)
-{
-	char	**tokens;
-	char	*path;
-	pid_t	pid;
-
-	tokens = split_with_quotes(cmd);
-	if (!tokens || !tokens[0])
-		return (perror("Comando vazio\n"));
-	pid = fork();
-	if (pid < 0)
-		return ;
-	path = search_valid_path(ft_split(cmd, ' ')[0], envp);
-	if (pid == 0)
-	{
-		valid_outfile_and_path(cmd, node, path);
-		if (execve(path, tokens, envp) == -1)
-			exit (127);
-		exit (0);
-	}
-	waitpid(pid, NULL, 0);
-}
-
 static char    *if_env_var(t_ast_node *node, char **tokens)
 {
     char    *cmd;
     char    *expanded;
-    int        i;
+    int		i;
 
     i = 0;
     cmd = process_env_var(tokens[0]);
@@ -84,6 +67,33 @@ static char    *if_env_var(t_ast_node *node, char **tokens)
     }
     return (cmd);
 }
+static void	execute_node_command(t_ast_node *node, char *cmd, char **envp)
+{
+	char	**tokens;
+	char	*path;
+	pid_t	pid;
+	char	*built[1];
+
+	built[0] = "PATH=built-ins";
+	tokens = split_with_quotes(cmd);
+	cmd = if_env_var(node, tokens);
+	if(if_cd(cmd, tokens, envp, node))
+		return ;
+	path = search_valid_path(ft_split(cmd, ' ')[0], built);
+	if(!path)
+		path = search_valid_path(ft_split(cmd, ' ')[0], envp);
+	pid = fork();
+	if (pid < 0)
+		return ;
+	if (pid == 0)
+	{
+		valid_outfile_and_path(cmd, node, path);
+		if (execve(path, tokens, envp) == -1)
+			exit (127);
+		exit (0);
+	}
+	waitpid(pid, NULL, 0);
+}
 
 static void	execute_word_node(t_ast_node *node, char *cmd, char **envp)
 {
@@ -94,17 +104,15 @@ static void	execute_word_node(t_ast_node *node, char *cmd, char **envp)
 
 	built[0] = "PATH=built-ins";
 	tokens = split_with_quotes(cmd);
-	if (!tokens || !tokens[0])
-		return (perror("Comando vazio\n"));
-	if(if_cd(cmd, tokens, envp))
-		return ;
-	pid = fork();
-	if (pid < 0)
-		return ;
 	cmd = if_env_var(node, tokens);
 	path = search_valid_path(ft_split(cmd, ' ')[0], built);
 	if(!path)
 		path = search_valid_path(ft_split(cmd, ' ')[0], envp);
+	if(if_cd(cmd, tokens, envp, node))
+		return ;
+	pid = fork();
+	if (pid < 0)
+		return ;
 	if (pid == 0)
 	{
 		valid_outfile_and_path(cmd, node, path);
@@ -118,6 +126,10 @@ static void	execute_word_node(t_ast_node *node, char *cmd, char **envp)
 void	execute_command(char *cmd, char **envp,
 			t_ast_node *node)
 {
+	if (node->outfile)
+		dup2(node->outfile, STDOUT_FILENO);
+	if (node->infile)
+		dup2(node->infile, STDIN_FILENO);
 	if (node->type == NODE_COMMAND)
 		execute_node_command(node, cmd, envp);
 	else if (node->type == TOKEN_WORD)
